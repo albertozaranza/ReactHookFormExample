c053d4294cfc343f33d23a950ac43a8c547195e5
Alberto Zaranza <albertozaranza@ioasys.com.br>
2020-07-07 00:01:35 -0300

    create interface

--- App.js
+++ App.js
@@ -1,113 +1,63 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+import React, {useRef} from 'react';
 import {
   SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
   Text,
-  StatusBar,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
+  const passwordRef = useRef();
 
-const App: () => React$Node = () => {
+  const {center, inputContainer, button, title} = styles;
+
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <SafeAreaView style={center}>
+      <Text style={title}>React Hook Form</Text>
+      <Text>E-mail:</Text>
+      <TextInput
+        style={inputContainer}
+        keyboardType="email-address"
+        onSubmitEditing={() => passwordRef.current.focus()}
+        returnKeyType="next"
+      />
+      <Text>Senha:</Text>
+      <TextInput style={inputContainer} secureTextEntry ref={passwordRef} />
+      <TouchableOpacity style={button} onPress={() => {}}>
+        <Text>Entrar</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  title: {
+    width: '100%',
+    marginBottom: 48,
+    textAlign: 'center',
+    fontSize: 32,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignContent: 'center',
+    paddingHorizontal: 16,
   },
-  body: {
-    backgroundColor: Colors.white,
+  inputContainer: {
+    marginVertical: 8,
+    paddingHorizontal: 8,
+    borderWidth: 1,
+    borderRadius: 8,
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  button: {
+    width: '100%',
+    height: 48,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 16,
+    borderRadius: 8,
+    backgroundColor: '#ccc',
   },
 });
 
